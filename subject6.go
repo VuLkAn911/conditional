4aa98582ceb48e4f7083f9673e1654d569137ff6
VuLkAn911 <donkikon828@gmail.com>
2026-02-20 20:45:49 +0500

    ababababab

---
+++ subject6.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	// sub 1
+	fmt.Println(5 == 5)                 // true
+	fmt.Println(10 != 3)                // true
+	fmt.Println(7 > 12)                 //false
+	fmt.Println(15 < 20)                // true
+	fmt.Println(8 >= 8)                 // true
+	fmt.Println(6 <= 4)                 // false
+	fmt.Println((10 > 5) && (3 < 1))    // false
+	fmt.Println((10 > 5) || (3 < 1))    // true
+	fmt.Println(!(5 == 5))              //false
+	fmt.Println(!(7 < 3))               // true
+	fmt.Println(true && false)          // false
+	fmt.Println(true || false)          // true
+	fmt.Println((4+6 == 10) && (9 > 2)) // true
+	fmt.Println((12/3 == 4) || (8 < 5)) // true
+
+	// sub 2
+
+	age := 25
+	hasTicket := true
+
+	canEnter := (age >= 18) && hasTicket
+	fmt.Printf("Can enter: %t\n", canEnter)
+
+	//sub 3
+
+	var isLoggedIn bool
+	var isAdmin bool
+
+	hasAccess := true
+	isAdmin = hasAccess && isLoggedIn || isAdmin
+	fmt.Printf("Has access: %t\n", hasAccess)
+}
